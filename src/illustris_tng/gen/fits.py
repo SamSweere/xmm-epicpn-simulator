7a9dd834b8ac09c9ccbeb43f12ae818a26285813
Bojan Todorkov <bojan@todorkov.de>
2023-10-25 15:28:14 +0200

    Supress any logging from yt

--- src/illustris_tng/gen/fits.py
+++ src/illustris_tng/gen/fits.py
@@ -1,3 +1,5 @@
+import os
+from contextlib import redirect_stderr, redirect_stdout
 from datetime import datetime
 from pathlib import Path
 from typing import Dict, List
@@ -30,41 +32,45 @@ def cutout_to_xray_fits(
                     f"width={width}; resolution={resolution}; redshift={redshift}; overwrite={overwrite}; "
                     f"output_dir={output_dir}; cutout_datafolder={cutout.parent}")
         start = datetime.now()
-        ds = yt.load(f"{cutout.resolve()}", default_species_fields="ionized")
 
-        yt.add_xray_emissivity_field(ds, emin, emax, redshift, data_dir=cloudy_emissivity_root)
+        # Force turn off all logging from yt via redirecting stdout and stderr into /dev/null
+        with open(os.devnull, "w") as f, redirect_stdout(f), redirect_stderr(f):
+            ds = yt.load(f"{cutout.resolve()}", default_species_fields="ionized")
 
-        for mode, normals in mode_dict.items():
-            for normal in normals:
-                for w in width:
-                    for r in resolution:
-                        normal_str = f"{normal[0]}_{normal[1]}_{normal[2]}" if mode == "proj" else normal
-                        filename = f"{cutout.name.replace('.hdf5', '')}_m_{mode}_r_{r}_w_{w}_n_{normal_str}"
+            yt.add_xray_emissivity_field(ds, emin, emax, redshift, data_dir=cloudy_emissivity_root)
 
-                        fits_filename = filename + '.fits'
-                        fits_path = output_dir / fits_filename
+            for mode, normals in mode_dict.items():
+                for normal in normals:
+                    for w in width:
+                        for r in resolution:
+                            normal_str = f"{normal[0]}_{normal[1]}_{normal[2]}" if mode == "proj" else normal
+                            filename = f"{cutout.name.replace('.hdf5', '')}_m_{mode}_r_{r}_w_{w}_n_{normal_str}"
 
-                        if fits_path.exists() and not overwrite:
-                            raise FileExistsError(f"{fits_path} already exists and `overwrite` is False")
-                        if mode == "proj":
-                            yt_fits = yt.FITSOffAxisProjection(ds, normal=normal,
-                                                               fields=('gas', 'xray_photon_intensity_0.5_2.0_keV'),
-                                                               center=(sub['pos_x'], sub['pos_y'], sub['pos_z']),
-                                                               width=(w, "code_length"), image_res=r)
-                        else:
-                            yt_fits = yt.FITSSlice(ds, axis=normal, fields=('gas', 'xray_photon_intensity_0.5_2.0_keV'),
-                                                   center=(sub['pos_x'], sub['pos_y'], sub['pos_z']),
-                                                   width=(w, "code_length"), image_res=r)
+                            fits_filename = filename + '.fits'
+                            fits_path = output_dir / fits_filename
 
-                        yt_fits.update_header(field="all", key="NORMAL", value=str(normal))
-                        yt_fits.update_header(field="all", key="WIDTH", value=w)
-                        yt_fits.update_header(field="all", key="REDSHIFT", value=redshift)
-                        yt_fits.update_header(field="all", key="EMIN", value=emin)
-                        yt_fits.update_header(field="all", key="EMAX", value=emax)
+                            if fits_path.exists() and not overwrite:
+                                raise FileExistsError(f"{fits_path} already exists and `overwrite` is False")
+                            if mode == "proj":
+                                yt_fits = yt.FITSOffAxisProjection(ds, normal=normal,
+                                                                   fields=('gas', 'xray_photon_intensity_0.5_2.0_keV'),
+                                                                   center=(sub['pos_x'], sub['pos_y'], sub['pos_z']),
+                                                                   width=(w, "code_length"), image_res=r)
+                            else:
+                                yt_fits = yt.FITSSlice(ds, axis=normal,
+                                                       fields=('gas', 'xray_photon_intensity_0.5_2.0_keV'),
+                                                       center=(sub['pos_x'], sub['pos_y'], sub['pos_z']),
+                                                       width=(w, "code_length"), image_res=r)
 
-                        yt_fits.writeto(str(fits_path.resolve()), overwrite=overwrite)
+                            yt_fits.update_header(field="all", key="NORMAL", value=str(normal))
+                            yt_fits.update_header(field="all", key="WIDTH", value=w)
+                            yt_fits.update_header(field="all", key="REDSHIFT", value=redshift)
+                            yt_fits.update_header(field="all", key="EMIN", value=emin)
+                            yt_fits.update_header(field="all", key="EMAX", value=emax)
+
+                            yt_fits.writeto(str(fits_path.resolve()), overwrite=overwrite)
         end = datetime.now()
         logger.info(f"DONE\tProcessing of {cutout} took {end - start}.")
 
     except Exception as e:
-        logger.exception(f" ERROR, failed to process {cutout.name} with errro: {e}")
+        logger.exception(f" ERROR\tFailed to process {cutout.name} with errro: {e}")
