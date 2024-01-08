489afc1f8cd50396c2d53e21d1305b1f09b71dc1
Bojan Todorkov <bojan@todorkov.de>
2024-01-08 21:26:23 +0100

    Switch to using CLI version of tar instead of tarfile

--- src/xmm_utils/file_utils.py
+++ src/xmm_utils/file_utils.py
@@ -1,8 +1,7 @@
 import gzip
-import os
 import shutil
-import tarfile
 from pathlib import Path
+from src.xmm_utils.external_run import run_command
 
 
 # Level 5 is a good balance between speed and compression ratio for my usecase
@@ -13,28 +12,11 @@ def compress_gzip(in_file_path, out_file_path, compresslevel=6):
 
 
 def compress_targz(in_file_path: Path, out_file_path: Path):
-    with tarfile.open(out_file_path, "w:gz", compresslevel=6) as tar:
-        tar.add(in_file_path)
+    run_command(f"tar -czf {out_file_path.resolve()} {in_file_path.resolve()}")
 
 
 def decompress_targz(in_file_path: Path, out_file_dir: Path):
     out_file_dir.mkdir(parents=True, exist_ok=True)
-    with tarfile.open(in_file_path, "r:gz") as tar:
-
-        def is_within_directory(directory: Path, target: Path):
-            abs_directory = directory.absolute()
-            abs_target = target.absolute()
-
-            prefix = Path(os.path.commonpath([abs_directory, abs_target]))
-
-            return prefix == abs_directory
-
-        def safe_extract(tar: tarfile.TarFile, path: Path = Path(".")):
-            for member in tar.getmembers():
-                member_path = path / member.name
-                if not is_within_directory(path, member_path):
-                    raise Exception("Attempted Path Traversal in Tar File")
-
-            tar.extractall(path)
-
-        safe_extract(tar, path=out_file_dir)
+    run_command(
+        f"tar -xzf {in_file_path.resolve()} -C {out_file_dir.resolve()} --strip-components=1"
+    )
