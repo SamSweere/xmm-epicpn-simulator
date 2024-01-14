2dab719d507fa147d95c09b8d9d2b031e478df23
Bojan Todorkov <bojan@todorkov.de>
2024-01-14 15:31:59 +0100

    Change log level of shell commands

--- src/xmm_utils/external_run.py
+++ src/xmm_utils/external_run.py
@@ -4,16 +4,19 @@ from typing import Optional
 from loguru import logger
 
 
-def run_command(cmd, cmd_input: Optional[str] = None, verbose=True) -> None:
-    if verbose:
-        logger.info(f"Running command:\n\t{cmd}")
-    #
+def run_command(cmd, cmd_input: Optional[str] = None) -> None:
+    logger.debug(f"Running command:\n\t{cmd}")
     # Execute a shell command with the stdout and stderr being redirected to a log file
-    #
     if cmd_input is not None:
-        cmd_input = bytes(cmd_input, 'ascii')
-    result = subprocess.run(args=cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, input=cmd_input,
-                            close_fds=True)
+        cmd_input = bytes(cmd_input, "ascii")
+    result = subprocess.run(
+        args=cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        input=cmd_input,
+        close_fds=True,
+    )
     retcode = result.returncode
     if retcode != 0:
         raise RuntimeError(f"Execution of {cmd} returned {retcode}\n{result.stdout}")
